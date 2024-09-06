d13e6a624cabdb0e602281a666fc4d594bc61384
Gauri Shirkande <gaurishirkande11111@gmail.com>
2024-09-06 14:29:20 +0530

    Update userdata.sh

--- jenkins-sonarqube-trivy-server/userdata.sh
+++ jenkins-sonarqube-trivy-server/userdata.sh
@@ -9,7 +9,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
+sudo dnf install java-17-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
